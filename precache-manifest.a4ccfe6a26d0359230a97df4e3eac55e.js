cdc47cab5415fa69c5b4dced503d6eb8e778d915
Code Sandbox <hello@codesandbox.io>
2023-11-10 02:44:23 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.a4ccfe6a26d0359230a97df4e3eac55e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b28d289840b9b8f25aa352550ab1dd8c",
+    "url": "https://freddypinto.github.io/csb-gw68sp/index.html"
+  },
+  {
+    "revision": "9b86cda34471ddc256e6",
+    "url": "https://freddypinto.github.io/csb-gw68sp/static/css/main.8aa0043f.chunk.css"
+  },
+  {
+    "revision": "47fd0ae98774a5a2e826",
+    "url": "https://freddypinto.github.io/csb-gw68sp/static/js/2.89a41c09.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://freddypinto.github.io/csb-gw68sp/static/js/2.89a41c09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b86cda34471ddc256e6",
+    "url": "https://freddypinto.github.io/csb-gw68sp/static/js/main.57aaa697.chunk.js"
+  },
+  {
+    "revision": "d101011db2a52a820768",
+    "url": "https://freddypinto.github.io/csb-gw68sp/static/js/runtime-main.d6525aa4.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "https://freddypinto.github.io/csb-gw68sp/precache-manifest.a4ccfe6a26d0359230a97df4e3eac55e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://freddypinto.github.io/csb-gw68sp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
